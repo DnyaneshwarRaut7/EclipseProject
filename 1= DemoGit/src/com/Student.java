7d97608796a938ae8ba5ddd0eb7064005f49ecf8
DnyaneshwarRaut7 <123471624+DnyaneshwarRaut7@users.noreply.github.com>
2023-01-26 15:13:08 +0530

    Student data set..

---
+++ 1= DemoGit/src/com/Student.java
@@ -0,0 +1,40 @@
+package com;
+
+public class Student {
+
+	private int id;
+
+	private String sname;
+
+	private String Address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", sname=" + sname + ", Address=" + Address + "]";
+	}
+
+}
--- 1= DemoGit/src/com/Test.java
+++ 1= DemoGit/src/com/Test.java
@@ -4,10 +4,16 @@ public class Test {
 
 	public static void main(String[] args) {
 
-	
-	System.out.println("First Project convert in Github");
-	
-	
+		System.out.println("First Project convert in Github");
+		
+		Student s = new Student();
+		
+		s.setId(11);
+		s.setSname("Mauli");
+		s.setAddress("Pune");
+
+		
+		System.out.println("Check Student Data :- "+ s);
 	}
 
 }
