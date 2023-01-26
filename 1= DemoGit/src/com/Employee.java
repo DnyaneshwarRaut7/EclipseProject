b52ce5b1e549b0b38e5120919a0900515ce7a324
DnyaneshwarRaut7 <123471624+DnyaneshwarRaut7@users.noreply.github.com>
2023-01-26 16:14:31 +0530

    Create   Employee.java

---
+++ 1= DemoGit/src/com/Employee.java
@@ -0,0 +1,29 @@
+public class Employee{
+
+private int id;
+  
+private String ename;
+  
+private String address;
+  
+public void setId(int id){
+this.id=id;
+}
+  public int getId(){
+  return id;
+  } 
+  public void setEname(String ename){
+  this.ename=ename;
+  }
+  public Sting getEname(){
+    return ename;
+  }
+  public void setAddress(String address){
+  this.address=address;
+  }
+  public Sting getAddress(){
+    return ename;
+  }
+  
+
+}
