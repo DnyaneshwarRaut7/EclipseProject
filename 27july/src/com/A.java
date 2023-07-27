af4101a7d7a17e4418a4937442c8ce79b6c9d359
DnyaneshwarRaut7 <123471624+DnyaneshwarRaut7@users.noreply.github.com>
2023-07-27 15:04:17 +0530

    change

--- 27july/src/com/A.java
+++ 27july/src/com/A.java
@@ -5,6 +5,8 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println(" hi hello");
 		System.out.println("how are you");
+		
+		System.out.println("I am Dnyaneshwar Sambhaji Raut");
 	}
 
 }
