b54c0f02bcbda70e4258b4c3b7c0b9cca56024fe
DnyaneshwarRaut7 <123471624+DnyaneshwarRaut7@users.noreply.github.com>
2023-07-27 15:44:12 +0530

    change

--- 27july/src/com/A.java
+++ 27july/src/com/A.java
@@ -7,7 +7,7 @@ public class A {
 		System.out.println("how are you");
 		
 		System.out.println("I am Dnyaneshwar Sambhaji Raut");
-		System.out.println("Yes I Have a complited My Graution BCA From SPPU in 2022 with cgpa 8.95")
+		System.out.println("Yes I Have a complited My Graution BCA From SPPU in 2022 with cgpa 8.95");
 	}
 
 }
